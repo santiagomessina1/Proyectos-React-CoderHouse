5556116821b3fca59f9bb5f683c452bc76257c6c
santimessina <santiagomessina1@gmail.com>
2022-09-02 11:09:00 -0300

    update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,33 @@
 import React from "react";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Button from 'react-bootstrap/Button';
-const NavBar = () =>{
-    return(
-        <Navbar className='navIndex'>
-        <Container >
-          <Navbar.Brand href="#home" className='logo'>Blue <span>Label</span></Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className='links'>Home</Nav.Link>
-            <Nav.Link href="#features">Products</Nav.Link>
-            <Button variant="outline-dark">Shopping Cart</Button>{' '}
-          </Nav>
-        </Container>
-      </Navbar>
 
-        
-    )
+function NavBar() {
+  return (
+    <header className="headerInicio">
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <a className="navbar-brand text-light " href="#">Blue <span className="rojo">Label</span>
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              
+              <li className="nav-item">
+                <a className="nav-link text-light link--3" href="./paginas/precios.html">Home</a>
+                
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light link--3" href="./paginas/actividades.html">Products</a>
+              </li>
+  
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default NavBar
+export default NavBar;
+
