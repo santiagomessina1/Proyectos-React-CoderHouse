dbe49aad7859abb6165c46a47ae4e2b05da0b26d
santimessina <santiagomessina1@gmail.com>
2022-10-12 14:35:13 -0300

    itemCollection II

--- src/assets/Cart.js
+++ src/assets/Cart.js
@@ -120,4 +120,3 @@ const Cart = () => {
 export default Cart
 
 
-
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import ItemDetail from './ItemDetail'
 import { useParams } from 'react-router-dom'
-import { db } from '../Firebase/firebase'
 import { doc, getDoc, getFirestore } from 'firebase/firestore'
 
 
